50eb7a4b1f94daecb77e0812d2e4b289d666facc
Csalex01 <csillagalex2001@gmail.com>
2021-05-19 20:21:42 +0300

    Initial Commit

---
+++ Dictionary/cmake-build-debug/CMakeFiles/Dictionary.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Dictionary.dir/main.c.obj"
+  "Dictionary.exe"
+  "Dictionary.exe.manifest"
+  "Dictionary.pdb"
+  "libDictionary.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Dictionary.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
